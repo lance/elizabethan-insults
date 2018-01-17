3d10903cd11c33c315762a1b44a25b2f82d45043
Lance Ball <lball@redhat.com>
2018-01-17 16:45:28 -0500

    src: add swagger API documentation and licenses

--- adjective/index.js
+++ adjective/index.js
@@ -1,4 +1,5 @@
 'use strict';
+const path = require('path');
 
 const express = require('express');
 const probe = require('kube-probe');
@@ -6,6 +7,7 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
 
 // add the API
 router.get('/adjective', require('./lib/adjective'));
@@ -16,6 +18,13 @@ probe(app, {
   readinessURL: '/health/readiness',
   livenessURL: '/health/liveness'
 });
+
+// add swagger API docs
+app.use('/api-docs', swaggerUi.serve,
+  swaggerUi.setup(require('./lib/adjective.json')));
+
+// serve licenses/licenses.html from the file system
+app.use('/licenses', express.static(path.join(__dirname, 'licenses')));
 
 app.listen(port);
 console.log(`adjective service listening on ${port}`);
