3aaed7b4ec9af5f670b0f2c603301bb23bf1a8f8
Lance Ball <lball@redhat.com>
2019-09-10 13:43:37 -0400

    src: uncomment create service and deployment commands

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,19 @@
 
 # ./docker.sh
 
-# kubectl create deployment adjective --image=lanceball/insults-adjective:v0.0.1
-# kubectl expose deployment adjective --port=8080 --target-port=8080 --type=NodePort
+kubectl create deployment adjective --image=lanceball/insults-adjective:v0.0.1
+kubectl expose deployment adjective --port=8080 --target-port=8080 --type=NodePort
 
-# kubectl create deployment noun --image=lanceball/insults-noun:v0.0.1
-# kubectl expose deployment noun --port=8080 --target-port=8080 --type=NodePort
+kubectl create deployment noun --image=lanceball/insults-noun:v0.0.1
+kubectl expose deployment noun --port=8080 --target-port=8080 --type=NodePort
 
-# kubectl create deployment insult --image=lanceball/insults-insult:v0.0.1
-# kubectl expose deployment insult --port=8080 --target-port=8080 --type=NodePort
+kubectl create deployment insult --image=lanceball/insults-insult:v0.0.1
+kubectl expose deployment insult --port=8080 --target-port=8080 --type=NodePort
 
-# kubectl create deployment frontend --image=lanceball/insults-frontend:v0.0.1
-# kubectl expose deployment frontend --port=8080 --target-port=8080 --type=NodePort
+kubectl create deployment frontend --image=lanceball/insults-frontend:v0.0.1
+kubectl expose deployment frontend --port=8080 --target-port=8080 --type=NodePort
 
-export HOST=`oc whoami --show-console | sed 's/https\(.*\):443/http\1/'`
+export HOST=http://master.stlouis-8f20.open.redhat.com
 export PORT=`oc get svc/frontend -o jsonpath='{.spec.ports[0].nodePort}'`
 
 echo "Visit the front end at ${HOST}:${PORT}"
